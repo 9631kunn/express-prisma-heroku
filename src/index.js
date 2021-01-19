2e5d66bc8b15f421a2abc09ee151dc52528d7eb8
9631kunn <infourbh+1@gmail.com>
2021-01-19 15:02:23 +0900

    add: cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import pkgPrisma from "@prisma/client";
 import express from "express";
+import cors from "cors";
 
 const { PrismaClient } = pkgPrisma;
 const prisma = new PrismaClient();
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
+const corsOptions = {
+  origin: "https://31navi.com",
+  optionsSuccessStatus: 200,
+};
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -13,12 +21,12 @@ app.get("/", (req, res) => {
 
 // USER
 
-app.get("/users", async (req, res, next) => {
+app.get("/users", cors(corsOptions), async (req, res, next) => {
   const users = await prisma.user.findMany();
   return res.json(users);
 });
 
-app.get("/user/:id", async (req, res, next) => {
+app.get("/user/:id", cors(corsOptions), async (req, res, next) => {
   const id = req.params.id;
   const user = await prisma.user.findUnique({
     where: {
@@ -28,7 +36,7 @@ app.get("/user/:id", async (req, res, next) => {
   res.json(user);
 });
 
-app.post("/user", async (req, res, next) => {
+app.post("/user", cors(corsOptions), async (req, res, next) => {
   const data = { ...req.body };
   const newUser = await prisma.user.create({
     data: data,
@@ -38,12 +46,12 @@ app.post("/user", async (req, res, next) => {
 
 // POST
 
-app.get("/posts/", async (req, res, next) => {
+app.get("/posts/", cors(corsOptions), async (req, res, next) => {
   const posts = await prisma.post.findMany();
   return res.json(posts);
 });
 
-app.get("/post/:id", async (req, res) => {
+app.get("/post/:id", cors(corsOptions), async (req, res) => {
   const { id } = req.params;
   const post = await prisma.post.findUnique({
     where: {
