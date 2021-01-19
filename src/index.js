d5b63c2294194f802d98145c75b5493b4e39cd28
9631kunn <infourbh+1@gmail.com>
2021-01-19 14:26:27 +0900

    add: create user

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const app = express();
 
 app.use(express.json());
 
+// USER
+
 app.get("/users", async (req, res, next) => {
   const users = await prisma.user.findMany();
   return res.json(users);
@@ -22,6 +24,16 @@ app.get("/user/:id", async (req, res, next) => {
   });
   res.json(user);
 });
+
+app.post("/user", async (req, res, next) => {
+  const data = { ...req.body };
+  const newUser = await prisma.user.create({
+    data: data,
+  });
+  res.json(newUser);
+});
+
+// POST
 
 app.get("/posts/", async (req, res, next) => {
   const posts = await prisma.post.findMany();
