d6b452fc33abf84fd8a0ec09e50f3ffe02223792
9631kunn <infourbh+1@gmail.com>
2021-01-19 13:28:41 +0900

    add: api end point

---
+++ README.md
@@ -0,0 +1,11 @@
+## Prisma Docs
+
+https://www.prisma.io/docs/
+
+## Postgres command cheat sheet for mac
+
+https://qiita.com/rasedevapp/items/ee186db5c8f7d9f7f292
+
+## GUI Database
+
+`$ npx prisma studio`
--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
-import pkg from "@prisma/client";
+import pkgPrisma from "@prisma/client";
+import express from "express";
 
-const { PrismaClient } = pkg;
+const { PrismaClient } = pkgPrisma;
 const prisma = new PrismaClient();
 
-async function main() {
-  const newUser = await prisma.user.create({
-    data: {
-      name: "9631kunn",
-      email: "9631kunn@test.com",
-      posts: {
-        create: {
-          title: "Hello World",
-        },
-      },
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async (req, res, next) => {
+  const users = await prisma.user.findMany();
+  return res.send(users);
+});
+
+app.get("/posts/", async (req, res, next) => {
+  const posts = await prisma.post.findMany();
+  return res.send(posts);
+});
+
+app.get("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const post = await prisma.post.findUnique({
+    where: {
+      id: Number(id),
     },
   });
-  console.log("Created new user:", newUser);
+  res.json(post);
+});
 
-  const allUsers = await prisma.user.findMany({
-    include: { posts: true },
-  });
-
-  console.log("All: users");
-  console.dir(allUsers, { depth: null });
-}
-
-main()
-  .catch((e) => console.error(e))
-  .finally(async () => await prisma.$disconnect());
+app.listen(3000);
